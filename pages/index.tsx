c91c782d3e4570e9ebe09039af133740bb991ff4
Yuji Nakamura <ny5117@icloud.com>
2022-04-03 17:47:17 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Meduum Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>This is Medium 2.0</h1>
+    </div>
+  )
+}
+
+export default Home
